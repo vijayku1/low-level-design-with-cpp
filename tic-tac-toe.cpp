4b712914291dfa25e4d770230657953ce4090f65
VIJAY KUMAR <87418015+vijayku1@users.noreply.github.com>
2022-06-21 00:33:49 +0530

    commit first

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 class TicTacToe
 {
-    int board[][];
 
 public:
     TicTacToe(int n)
@@ -12,7 +11,7 @@ public:
     int move(int row, int col, int player)
     {
         board[row][col] = player;
-        if (checkrow(row, player) || check(col, player) || (row == col && checkmaindiagonal(player) || (row + col == board.size() - 1) && checkOppositeDiagonal(player)))
+        if (checkrow(row, player) || checkcol(col, player) || (row == col && checkmaindiagonal(player) || (row + col == board.size() - 1) && checkOppositeDiagonal(player)))
         {
             return player;
         }
@@ -22,8 +21,49 @@ public:
 private:
     bool checkrow(int row, int player)
     {
+        for (int j = 0; j < board[0].size(); j++)
+        {
+            if (broad[row][j] != player)
+                return false;
+        }
+        return true;
     }
-} int main()
+    bool checkcol(int col, int player)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            if (board[i][col] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkmaindiagonal(int player)
+    {
+        for (int i = 0; i < board.size(); i++)
+        {
+            if (board[i][i] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkOppositeDiagonal(int player)
+    {
+        for (int i = board[0].size() - 1; i >= 0; i--)
+        {
+            if (board[i][i] != player)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main()
 {
 
     return 0;
